d09d7a23e3b9b15f16f1b9e9783523496da13481
exit1100 <48817138+exit1100@users.noreply.github.com>
2019-03-31 00:40:00 +0900

    Add files via upload

---
+++ 10950.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+	int A, B, T, i;
+	
+	scanf("%d",&T);
+	int result[T];
+	
+	for(i=0;i<=T-1;i++){
+		scanf("%d %d",&A, &B);
+		result[i]=A+B;
+	}
+	for(i=0;i<=T-1;i++){
+		printf("%d\n",result[i]);
+	}
+
+	return 0;
+}
